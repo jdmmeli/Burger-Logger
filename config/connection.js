e745a551b719f5049e5bbe2dd03108bd04176e79
Jason Meli <jdmmeli@yahoo.com>
2020-05-12 18:18:40 -0400

    method-override

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection;
 if (process.env.JAWSDB_URL) {
 	connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
+  connection = mysql.createConnection({
 	host: "localhost",
 	user: "root",
 	password: "1Grondster43!",
