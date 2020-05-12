12a6ffbaa845741bf5a10a4df2bb9db9155a70d2
Jason Meli <jdmmeli@yahoo.com>
2020-05-12 12:33:03 -0400

    trying to deploy heroku

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
 	host: "localhost",
 	user: "root",
-	password: "",
+	password: "1Grondster43!",
 	database: "burgers_db"
 });
 };
